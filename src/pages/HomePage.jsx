53eefa471569ab56a8133cb74fa820ee362878a5
Kishore2268 <kishore@anivarti.in>
2025-02-03 12:11:14 +0530

    first commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -80,10 +80,10 @@ const HomePage = () => {
             {/* Service 1 */}
             <div className="bg-white p-8 rounded-lg text-center shadow-lg border border-gray-200 transition duration-300 hover:scale-105 hover:shadow-xl hover:border-2 hover:border-electricBlue">
               <FaShoppingCart className="text-electricBlue text-3xl lg:text-5xl mx-auto" />
-              <h1 className="mt-4 text-lg lg:text-2xl font-light text-gray-600 tracking-[3px]">
+              <h1 className="mt-4 text-lg lg:text-xl font-light text-gray-600 tracking-[3px]">
                 E-COMMERCE
               </h1>
-              <p className="mt-2 text-md lg:text-lg text-gray-600 text-start">
+              <p className="mt-2 text-md tracking-[1px] text-gray-500 text-start">
                 Comprehensive services for marketplace onboarding and seller
                 portal management to maximize ROI.
               </p>
@@ -91,10 +91,10 @@ const HomePage = () => {
             {/* Service 2 */}
             <div className="bg-white p-8 rounded-lg text-center shadow-lg border border-gray-200 transition duration-300 hover:scale-105 hover:shadow-xl hover:border-2  hover:border-electricBlue">
               <FaBullhorn className="text-electricBlue text-3xl lg:text-5xl mx-auto" />
-              <h1 className="mt-4 text-lg lg:text-2xl font-light text-gray-600 tracking-[3px]">
+              <h1 className="mt-4 text-lg lg:text-xl font-light text-gray-600 tracking-[3px]">
                 BRANDING & MARKETING
               </h1>
-              <p className="mt-2 text-md lg:text-lg text-gray-600 text-start">
+              <p className="mt-2 text-md tracking-[1px] text-gray-500 text-start">
                 Developing strategies to boost brand value and manage ad
                 campaigns for maximizing ROAS.
               </p>
@@ -102,10 +102,10 @@ const HomePage = () => {
             {/* Service 3 */}
             <div className="bg-white p-8 rounded-lg text-center shadow-lg border border-gray-200 transition duration-300 hover:scale-105 hover:shadow-xl hover:border-2  hover:border-electricBlue">
               <FaCreditCard className="text-electricBlue text-3xl lg:text-5xl mx-auto" />
-              <h1 className="mt-4 text-lg lg:text-2xl font-light text-gray-600 tracking-[3px]">
+              <h1 className="mt-4 text-lg font-light text-gray-600 tracking-[3px]">
                 PAYMENT
               </h1>
-              <p className="mt-2 text-md lg:text-lg text-gray-600 text-start">
+              <p className="mt-2 text-md tracking-[1px] text-gray-500 text-start">
                 Integrating top-tier payment gateways to ensure smooth
                 transactions and prevent lost sales.
               </p>
@@ -113,10 +113,10 @@ const HomePage = () => {
             {/* Service 4 */}
             <div className="bg-white p-8 rounded-lg text-center shadow-lg border border-gray-200 transition duration-300 hover:scale-105 hover:shadow-xl hover:border-2  hover:border-electricBlue">
               <FaTruckMoving className="text-electricBlue text-3xl lg:text-5xl mx-auto" />
-              <h1 className="mt-4 text-lg lg:text-2xl font-light text-gray-600 tracking-[3px]">
+              <h1 className="mt-4 text-lg lg:text-xl font-light text-gray-700 tracking-[3px]">
                 LOGISTICS
               </h1>
-              <p className="mt-2 text-md lg:text-lg text-gray-600 text-start">
+              <p className="mt-2 text-md tracking-[1px] text-gray-500 text-start">
                 Partnering with logistics experts for efficient, last-mile
                 global delivery solutions.
               </p>
@@ -124,10 +124,10 @@ const HomePage = () => {
             {/* Service 5 */}
             <div className="bg-white p-8 rounded-lg text-center shadow-lg border border-gray-200 transition duration-300 hover:scale-105 hover:shadow-xl hover:border-2  hover:border-electricBlue">
               <FaSearchDollar className="text-electricBlue text-3xl lg:text-5xl mx-auto" />
-              <h1 className="mt-4 text-lg lg:text-2xl font-light text-gray-600 tracking-[3px]">
+              <h1 className="mt-4 text-lg lg:text-xl font-light text-gray-700 tracking-[3px]">
                 MARKET RESEARCH
               </h1>
-              <p className="mt-2 text-md lg:text-lg text-gray-600 text-start">
+              <p className="mt-2 text-md tracking-[2px]  text-gray-500 text-start">
                 Conducting market research to identify prime business
                 opportunities for your growth.
               </p>
@@ -135,10 +135,10 @@ const HomePage = () => {
             {/* Service 6 */}
             <div className="bg-white p-8 rounded-lg text-center shadow-lg border border-gray-200 transition duration-300 hover:scale-105 hover:shadow-xl hover:border-2  hover:border-electricBlue">
               <FaCalculator className="text-electricBlue text-3xl lg:text-5xl mx-auto" />
-              <h1 className="mt-4 text-lg lg:text-2xl font-light text-gray-600 tracking-[3px]">
+              <h1 className="mt-4 text-lg lg:text-xl font-light text-gray-700 tracking-[3px]">
                 ACCOUNTING
               </h1>
-              <p className="mt-2 text-md lg:text-lg text-gray-600 text-start">
+              <p className="mt-2 text-md tracking-[1px] text-gray-500 text-start">
                 Offering integrated return filing and accounting solutions, with
                 interactive reports.
               </p>
